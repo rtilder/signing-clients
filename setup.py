5871354513674779b6cf9dbc70b715e0ca1dd8aa
Ryan Tilder <ryan@tilder.org>
2012-11-16 14:26:23 -0800

    Versions are strings, Ryan

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(here, 'README.md')) as f:
 
 
 setup(name='signing_clients',
-    version=0.1.1,
+    version='0.1.1',
     description="Applications signature/manifest manipulator and receipt verifier",
     long_description=README,
     classifiers=[
